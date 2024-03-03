addeeee166461d4b23ff9e47db0ee5c35d41c28c
kopsis-net <kopsis.net@gmail.com>
2024-03-04 00:08:40 +0300

    modified:   public/index.js

--- public/index.js
+++ public/index.js
@@ -4,10 +4,6 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.EX_PORT;
-<<<<<<< HEAD
-app.use(cors());
-=======
->>>>>>> 3a84c3e (	modified:   frontend/src/app/app.component.html)
 app.use(cors({
   origin: '*'
 }));

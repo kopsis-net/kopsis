99f43518c4a03e56091c73729b037665af2c55b0
kopsis-net <kopsis.net@gmail.com>
2024-03-03 23:54:39 +0300

    modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.component.ts 	modified:   frontend/src/app/app.module.ts 	new file:   frontend/src/app/data.service.spec.ts 	new file:   frontend/src/app/data.service.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.js

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  data: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.dataService.getData().subscribe(response => {
+      this.data = response;
+    });
+  }
 }
--- public/index.js
+++ public/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.EX_PORT;
+app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+// Örnek veri
+const sampleData = { message: 'Hello from Express!' };
 
+// API endpoint'i oluşturma
 app.get('/', (req, res) => {
-  res.send('Merhaba Dünya! Nasılsınız');
+   // Örnek bir JSON yanıtı döndürme
+   const data = {
+    message: 'Merhaba, dünya!'
+  };
+
+  res.json(data);
 });
 
 app.listen(port, () => {
